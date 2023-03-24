d3a5b923a078e845bdb75a53358d257724bd71f7
Luke Harding <lmharding04@gmail.com>
2023-03-24 14:43:59 +0000

    Update / Added Javascipt / Makes cool prompts :D

--- scripts.js
+++ scripts.js
@@ -7,21 +7,21 @@ let time = prompt("Hello " + user + " Whats the time for you now? (0 - 23)");
 let message;
 
 if (time < 12) {
-    message = "Good morning " + user;
+    message = "Good morning! " + user + " Hope you have a good day!";
 } else if (time <= 18) {
-    message = "Good afternoon " + user;
+    message = "Good afternoon " + user + " You better be having a good day! -__-";
 } else if (time < 24) {
-    message = "Good night " + user;
+    message = "Good night " + user + " Now GO to bed! :/";
 } else {
     message = "That hour does not exist " + user;
 }
 
 console.log(message);
 
-let info = confirm("I am an egg wbu " + user + "?");
+let info = confirm("I am an egg, Are you an egg " + user + "?");
 
 if(info === true) {
-    console.log("Thats good" + user);
+    console.log("Thats good " + user);
 } else {
     console.log("Thats no good :/ " + user);
 }
