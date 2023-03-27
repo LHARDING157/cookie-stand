cc3f1e4c4647b113edba13351e91690c84414e19
Luke Harding <lmharding04@gmail.com>
2023-03-27 15:29:51 +0100

    update / Rating Stars :D

--- demo.js
+++ demo.js
@@ -1,1 +1,33 @@
 document.getElementById("demo").innerHTML = message;
+
+
+//for(initialization; CSSConditionRule; increment){
+// statements
+// }
+
+// string - sentence "tim"
+// number - 1
+// boolean - true or false
+// array - ["lou", "victor", "jeremy"]
+// object - {name: "lou", age: 21,}
+
+let myFavPets = ["dog", "cat", "goldfish"]
+
+for(let i = 0; i < myFavPets.length; 1++ ){
+    console.log("I love my pet " + myFavPets[i])
+}
+
+
+// while(condition){
+// statement
+//}
+
+let n = 0
+let x = 0
+
+while(n < 3){
+    n++
+    x += n
+    console.log(n)
+    console.log(x)
+}
--- learning.js
+++ learning.js
@@ -1,22 +1,49 @@
+let user;
+
 function yourName() {
-    let user = prompt("What's Your Name?")
-    return document.write(user)
+    user = prompt("What is your name?");
+    return document.write(user);
 }
 
+function guessANumber(){
+    let answer;
+
+    while(answer != 7){
+        answer= prompt("Guess a number between 1 - 10")
+
+        if(answer != 7){
+            alert("Try Again!")
+        } else {
+            alert("Good Job! That's Correct!")
+        }
+    }
+}
+
+guessANumber();
+
 function timeOfDay() {
-    let time = prompt("Hi " + user + " What time is it? (0 - 23)");
+    let time = prompt("Hi " + user + ", what hour is it? (0 - 23)");
 
     let message;
 
     if (time < 12) {
-        message = "Good Morning! " + user;
+    message = "Good morning " + user;
     } else if (time <= 18) {
-        message = "Good Afternoon! " + user;
-    } else if (time <= 18) {
-        message = "Good Night! " + user;
+    message = "Good afternoon " + user;
+    } else if (time < 24) {
+    message = "Good night " + user;
     } else {
-        message = "That hour doesn't exist! :D"
+    message = "That hour does not exist " + user;
     }
     console.log(message);
-    return(message);
+    return message;
+}
+
+function displayRating() {
+    let output = "";
+    let rating = prompt("Rate our cookies on a of Scale of 1-5, how many stars?");
+    for (let i = 0; i < rating; i++) {
+        output += "<img class='star' src='images/star.jpg' alt='rating star'>";
+    }
+    return document.write(output, " You Input " + rating + " Stars");
 }
